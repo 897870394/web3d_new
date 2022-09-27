22405694bbbc88c753b67356fdd7344c9b3dd8c3
hyc <13987647985@163.com>
2022-09-27 16:12:52 +0800

    change host

--- src/server/app.js
+++ src/server/app.js
@@ -59,7 +59,7 @@ app.post('/register', (req, res) => {
 });
 
 // type express.Server (http.Server)
-const server = app.listen(3000, 'localhost', () => {
+const server = app.listen(3000, '0.0.0.0', () => {
     const host = server.address().address;
     const port = server.address().port;
     console.log('Example app listening at http://%s:%s', host, port);
